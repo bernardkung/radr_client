14b8c7de450bca086e6e93096fb513e6b8869960
bernard kung <speak.freely@gmail.com>
2024-09-12 02:25:18 -0400

    added a cool radar loading animation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import './App.css'
 import * as d3 from "d3"
 import Dashboard from "./components/Dashboard"
+import Radar from "./components/Radar"
 import { useState, useEffect, useRef } from 'react'
 
 function App() {
   
   const [ loading, setLoading ] = useState(true)
   const [ data, setData ] = useState([])
-
+  const [ testData, setTestData ] = useState([])
 
 
   useEffect(()=>{
     Promise.all([
-      d3.json('http://127.0.0.1:8000/dashboard')
-    ]).then(([ res ])=>{
-      setData(res['data'])
+      d3.json('http://127.0.0.1:8000/dashboard'),
+      d3.json('http://127.0.0.1:8000/adrs?full=True'),
+    ]).then(([ res0, res1 ])=>{
+      setData(res0['data'])
+      setData(res1['data'])
       setLoading(false)
     })
   }, [])
 
+  return (
+    <div className="loading flexCol">
+      <Radar />
+      <p>Loading</p>
+    </div>)
+
   if (loading) {
-    return <>loading</>
+    return (
+    <div className="loading flexCol">
+      <Radar />
+      <p>Loading</p>
+    </div>)
   }
 
   return (
     <div className="App">
       <Dashboard data={ data } />
     </div>
+
   )
 }
 
---
+++ src/components/Radar.js
@@ -0,0 +1,12 @@
+import {ReactComponent as RadarSVG} from '../assets/satellite-dish-animation.svg'
+
+const Radar = ({})=>{
+  
+  return (
+    <div className="radar">
+      <RadarSVG />
+    </div>
+  )
+}
+
+export default Radar
