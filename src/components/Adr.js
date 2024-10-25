86ed24bd83d01536a20d17d2f0b97bf0b95713ab
bernard kung <speak.freely@gmail.com>
2024-10-25 18:05:04 -0400

    show list of events

--- src/components/Adr.js
+++ src/components/Adr.js
@@ -1,3 +1,4 @@
+import { memo, useMemo } from "react";
 
 export const Adr = ({ adrData }) => {
   if (!adrData) {
@@ -29,9 +30,25 @@ export const Adr = ({ adrData }) => {
     return tRows
   }
 
-  function generateTable(dict, dictName) {    
+
+  function guessType(event, capitalized=false) {
+    function capitalize(s) {
+      return String(s).charAt(0).toUpperCase() + String(s).slice(1);
+    }
+    const guess = Object.keys(event)[0].split("_")[0]
+    const formatGuess = capitalized
+      ? capitalize(guess)
+      : guess
+
+    console.log(Object.keys(event)[0], formatGuess)
+    return formatGuess
+  }
+
+  function generateTable(dict, dictName="") {    
+    if ( dictName=="" ) { dictName=guessType(dict, true)}
+
     const div = (
-      <div className={"tableContainer"} name={ dictName }>
+      <div className={"tableContainer flexCol"} name={ dictName }>
         <p className={"tableTitle"}>{ dictName }</p>
         <table className={ "tableTable" }>  
           <tbody className={ "tableBody" }>
@@ -44,27 +61,70 @@ export const Adr = ({ adrData }) => {
     return div
   }
 
-  function sortStages(stages) {
+  function sortStages(stages, asc=true) {
     const stageOrder =['45', '120', '180', 'ALJ']
     const ordering = {}
     for (let i = 0; i<stageOrder.length; i++) {
       ordering[stageOrder[i]] = i;
     }
 
-    return stages.sort( (a,b) => ordering[a.stage] - ordering[b.stage] )
+    return stages.sort( (a,b) => asc
+      ? ordering[a.stage] - ordering[b.stage] 
+      : ordering[b.stage] - ordering[a.stage] 
+    )
+  }
+
+  function sortEvents(a, b, asc=true) {
+    const aType = guessType(a)
+    const bType = guessType(b)
+    const aDateStr = aType + "_date"
+    const bDateStr = bType + "_date"
+    // const aDate = new Date(a[aDateStr])
+    // const bDate = new Date(b[bDateStr])
+    const order = a[aDateStr] >= b[bDateStr] 
+      ? 1 
+      : a[aDateStr] < b[bDateStr]
+        ? -1
+        : 0
+    return asc
+      ? order
+      : -order
+  }
+
+  function generateStagesTable(stages, asc=true) {
+    // Sort then map through each stage
+    return sortStages(stages, asc).map(stage=> { 
+      // Generate a table for the stage
+      const stageDiv = generateTable(stage, "Stage") 
+      // Generate events and a table for each event
+      const events = [...stage.submissions, ...stage.decisions]
+      const eventsDivs = events
+        .sort((a,b)=>sortEvents(a, b, asc))
+        .map(event => generateTable(event))
+
+      // Build output div
+      const divs = (
+        <div>
+          { stageDiv }
+          { eventsDivs }
+        </div>
+      )
+
+      return divs
+    })
   }
 
   const facilityDiv = generateTable(adrData.Facility, "Facility")
   const patientDiv = generateTable(adrData.Patient, "Patient")
   const adrDiv = generateTable(adrData.Adr, "ADR")
-  const stagesDiv = sortStages(adrData.Adr.stages).map(stage=> generateTable(stage, "Stage") )
+  const stagesDivs = generateStagesTable(adrData.Adr.stages, false)
   
   return (
     <div name={"adr_container"} className={"adrContainer"}>
       { facilityDiv }
       { patientDiv }
       { adrDiv }
-      { stagesDiv }
+      { stagesDivs }
     </div>
   )
 }
